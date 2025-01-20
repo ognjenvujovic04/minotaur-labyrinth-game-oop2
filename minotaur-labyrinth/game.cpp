37f5e0fe7f896aeb26e1ad458565705cc6d8efe3
ognjenvujovic04 <143551253+ognjenvujovic04@users.noreply.github.com>
2025-01-20 21:32:20 +0100

    Game add getMazeString, displayGameState, quit

--- minotaur-labyrinth/game.cpp
+++ minotaur-labyrinth/game.cpp
@@ -1,9 +1,65 @@
+#include <iostream>
 #include "game.h"
+#include "maze_genearator.h"
+
+using namespace std;
 
 Game::Game(int pRows, int pColumns, int pItemNumber) {
-	colomns = pColumns;
-	rows = pRows;
 	itemNumber = pItemNumber;
-	//mazeMatrix = maze_generator(pColumns, pRows, pItemNumber);
-	
+	mazeMatrix = generateMaze(pColumns, pRows, pItemNumber);
+	gameOver = false;
+}
+
+Game::~Game() {
+	for (int i = 0; i < sizeof(mazeMatrix); i++) {
+		delete[] mazeMatrix[i];
+	}
+	delete[] mazeMatrix;
+}
+
+void Game::start() {
+	char command;
+	while (!gameOver) {
+		displayGameState();
+		cout << "Koristite WASD za kretanje robota, Q za izlaz iz igre: ";
+		cout << "Unesite komandu: ";
+		cin >> command;
+		if (command == 'Q') {
+			quit();
+		}
+		else {
+			/*handleRobotMovement(command);
+			handleMinotaurMovement();*/
+		}
+	}
+}
+
+void Game::quit() {
+	gameOver = true;
+	//Todo zapis stanja
+}
+
+void Game::displayGameState() {
+	for (int i = 0; i < sizeof(mazeMatrix); i++) {
+		for (int j = 0; j < sizeof(mazeMatrix[i]); j++) {
+			cout << mazeMatrix[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+	//ispist broja predmeta i aktivnih predmeta
+	cout << "Broj predmeta: " << itemNumber << endl;
+	//todo ispis aktivnih predmeta i duzinu trajanja
+
+}
+
+string Game::getMazeString() {
+	string mazeString = "";
+	for (int i = 0; i < sizeof(mazeMatrix); i++) {
+		for (int j = 0; j < sizeof(mazeMatrix[i]); j++) {
+			mazeString += mazeMatrix[i][j];
+		}
+		mazeString += "\n";
+	}
+	return mazeString;
 }
--- minotaur-labyrinth/game.h
+++ minotaur-labyrinth/game.h
@@ -3,15 +3,24 @@
 using namespace std;
 
 class Game {
+private:
+	char** mazeMatrix;
+	int itemNumber;
+	//Robot robot;
+	//Minotaur minotaur;
+	bool gameOver;
+	
+	//void handleRobotMovement(char command); // Obrada unosa korisnika za robota
+	//void handleMinotaurMovement();         // Generisanje poteza Minotaura
+	//void handleItemPickup();               // Obrada podizanja predmeta
+
 public:
 	Game(int rows, int columns, int itemNumber);
 	~Game();
 
+	void start();
 	void quit();
+	void displayGameState();
+	string getMazeString();
 	
-	char** mazeMatrix;
-	int colomns, rows, itemNumber;
-
-
-
 };
