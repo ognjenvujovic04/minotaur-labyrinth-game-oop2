cebccaa944fa882ae1c560c5b78b58d54d69e170
ognjenvujovic04 <143551253+ognjenvujovic04@users.noreply.github.com>
2025-01-22 03:14:46 +0100

    Minimal dimension 7x7

--- minotaur-labyrinth/main.cpp
+++ minotaur-labyrinth/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <windows.h>
-#include "maze_genearator.h"
+#include "maze_generator.h"
 #include "game.h"
 
 using namespace std;
@@ -8,15 +8,27 @@ using namespace std;
 //Adrese varijabli koje se koriste u funkcijama
 void welcome(int& columns, int& rows, int& item_number) {
 	cout << "Dobrodosli u igricu bjeg iz lavirinta u Knososu!" << endl << endl;
-	cout << "Unesite broj kolona: ";
-	cin >> columns;
-	cout << "Unesite broj redova: ";
-	cin >> rows;
+
+	while (true) {
+		cout << "Unesite broj kolona: ";
+		cin >> columns;
+		cout << "Unesite broj redova: ";
+		cin >> rows;
+		if (rows < 7 || columns < 7) {
+			cout << "Dimenzije lavirinta moraju biti barem 7x7!" << endl;
+		}
+		else {
+			break;
+		}
+	}
 	while (true) {
 		cout << "Unesite broj predmeta: ";
 		cin >> item_number;
-		if (item_number > (columns * rows) / 2) {
+		// Broj polja u lavirintu je (columns - 1) * (rows - 1), oduzimam 2 za robota i minotaura, 
+		// i oduzimam (columns + rows) * 2 jer je to minilani broj blokoava
+		if (item_number > ((columns - 1) * (rows - 1) - 2) - ((columns + rows) * 2)) {
 			cout << "Broj predmeta ne moze biti veci od broja slobodnih polja lavirinta!" << endl;
+			cout << ((columns - 1) * (rows - 1) - 2) - ((columns + rows) * 2);
 		}
 		else if (item_number < 4) {
 			cout << "Broj predmeta mora biti veci od 3!" << endl;
@@ -25,25 +37,26 @@ void welcome(int& columns, int& rows, int& item_number) {
 			break;
 		}
 	}
-	cout << endl << "Igra pocinje za 3!" << endl;
+	/*cout << endl << "Igra pocinje za 3!" << endl;
 	Sleep(1000);
 	cout << "2!" << endl;
 	Sleep(1000);
 	cout << "1!" << endl;
 	Sleep(1000);
 	cout << "Sretno!" << endl;
-	Sleep(500);
+	Sleep(500);*/
 
 	//Brisanje teksta iz terminala
 	system("CLS");
+	
 }
 
 int main(){
 	int columns, rows, item_number;
 
-	welcome(columns, rows, item_number);
+	//welcome(columns, rows, item_number);
 	
-	Game game = Game(rows, columns, item_number);
+	Game game = Game(10, 10, 5);
 	game.start();
 
 }
